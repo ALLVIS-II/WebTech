54cf6d2a85b6d39f60d6d6a541bc7121075b44a5
ALLVIS-II <Alvislau2004@gmail.com>
2023-10-16 16:52:50 +1030

    a

---
+++ Week 2/pratical 1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Address (
+    adressID INT IDENTITY,
+    addressLine varchar(100) NOT NULL,
+    suburb varchar(50) NOT NULL,
+    postcode varchar(10) NOT NULL,
+    region varchar(50) NOT NULL,
+    country varchar(50) NOT NULL
+)
+
+CREATE TABLE Customers(
+    
+)
