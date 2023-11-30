565ac602fbaf33058621256624a678235ab7a71c
MochiJuki_ <73869314+ALLVIS-II@users.noreply.github.com>
2023-12-01 03:02:20 +1030

    Add files via upload

---
+++ part 2.sql
@@ -0,0 +1,35 @@
+Auctions(AuctionID, AuctionDate, AuctionType)
+PK(AuctionID)
+
+Address(addressID, addressID, addressLine, suburb, postcode, region, country)
+PK(addressID)
+
+AuctionsType(AuctionsTypeID, AuctionType)
+PK(AuctionsTypeID)
+FK(AuctionsType) ->Auctions(AuctionsType)
+
+Catalogues(CatalogueID, Name, Description, showcasephoto, Check)
+PK(CatalogueID)
+
+CatalogueType(CataloguetypeID, catalogueType)
+PK(CatalogueID)
+
+
+Bidders(BidderID, LotsID, AuctionID, BidderID, BidderAmount, BidderTime, isOnlineBidder, WinningBid)
+PK(BidderID)
+Fk(LotsID) -> Lotsders(LotsID)
+FK(AuctionID) -> Auctions(AuctionID)
+FK(BidderID) -> Bidders(BidderID)
+
+OnlineBidders(biddersID)
+PK(biddersID)
+
+OnfloorBidders(bidderID)
+PK(biddersID)
+
+
+sellers(sellerID, sellerName, CommissionRate, LottingFee)
+PK(sellerID)
+
+Lotsders(LotsID, Title, Description, minPrice, maxPrice)
+PK(LotsID)
